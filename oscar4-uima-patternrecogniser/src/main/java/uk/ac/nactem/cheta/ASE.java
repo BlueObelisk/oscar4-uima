0f92ce18e56cf03269870f35bf6eafb2d553d9d2
lh359 <lh359@cam.ac.uk>
2011-01-26 15:36:17 +0000

    success... tests for all uima classes work

---
+++ oscar4-uima-patternrecogniser/src/main/java/uk/ac/nactem/cheta/ASE.java
@@ -0,0 +1,61 @@
+
+
+/* First created by JCasGen Wed Jan 20 15:12:13 GMT 2010 */
+package uk.ac.nactem.cheta;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+
+
+/** enzyme
+ * Updated by JCasGen Mon Aug 16 14:00:43 BST 2010
+ * XML source: /home/lezan/galWorkspace/oscar4-uima/oscar4-uima-memmrecogniser/src/main/resources/uk/ac/nactem/cheta/uimaDescriptor/OscarMER1.xml
+ * @generated */
+public class ASE extends ChemicalEntity {
+  /** @generated
+   * @ordered 
+   */
+  public final static int typeIndexID = JCasRegistry.register(ASE.class);
+  /** @generated
+   * @ordered 
+   */
+  public final static int type = typeIndexID;
+  /** @generated  */
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected ASE() {}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public ASE(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public ASE(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public ASE(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {}
+     
+}
+
+    
---
+++ oscar4-uima-patternrecogniser/src/test/java/uk/ac/nactem/cheta/OscarUimaPatternTest.java
@@ -0,0 +1,149 @@
+package uk.ac.nactem.cheta;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.TypeSystemDescriptionFactory;
+
+import uk.ac.cam.ch.wwmm.oscar.Oscar;
+import uk.ac.cam.ch.wwmm.oscar.document.ITokenSequence;
+import uk.ac.cam.ch.wwmm.oscar.document.NamedEntity;
+import uk.ac.cam.ch.wwmm.oscarpattern.PatternRecogniser;
+import ch.unibe.jexample.Given;
+import ch.unibe.jexample.JExample;
+
+@RunWith(JExample.class)
+public class OscarUimaPatternTest {
+
+	private static String patternFolder = "uk/ac/nactem/cheta/patternrecogniser/";
+
+	@Test
+	public OscarUimaPattern testConstructor() throws Exception {
+		OscarUimaPattern oscarPattern = new OscarUimaPattern();
+		Assert.assertNotNull(oscarPattern);
+		return oscarPattern;
+	}
+
+	@Test
+	@Given("testConstructor")
+	/***************************************
+	 * Compares OscarMEMM to OscarUIMA-MEMM
+	 */
+	public String[] testUIMAPatternRecogniser(OscarUimaPattern uimaMEMM)
+			throws ValidityException, ParsingException, IOException, Exception {
+
+		File dir = new File(ClassLoader.getSystemResource(patternFolder)
+				.getFile());
+		OscarUIMATokenizer uimaTokenizer = new OscarUIMATokenizer();
+		String filenames[] = dir.list();
+		for (String tokeniseFile : filenames) {
+			JCas aJCas = createJCas();
+			String patternPath = patternFolder + tokeniseFile;
+            String docText = getContentAsString(patternPath);
+			
+			aJCas.setDocumentText(docText);
+			List<String> oscarTokenList = uimaTokenizer.getOscarTokens(docText);
+			uimaMEMM.convertOscarToSyntaxTokens(oscarTokenList, docText,
+					aJCas);
+			
+		    	
+
+			
+			List<NamedEntity> oscarEntities = getOscarEntities(docText);
+			
+			List<NamedEntity> uimaEntities = uimaMEMM.recogniseNEs(aJCas, docText);
+
+			
+//			writeToFile(tokeniseFile, uimaEntities);
+
+			Assert.assertEquals("UIMA and OSCAR are the same",
+					oscarEntities, uimaEntities);
+			
+
+		}
+		return filenames;
+	}
+
+	private void writeToFile(String tokeniseFile, List<NamedEntity> uimaEntities)
+			throws UnsupportedEncodingException, FileNotFoundException,
+			IOException {
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream("target/" + tokeniseFile), "UTF8"));
+
+		for (NamedEntity namedEntity : uimaEntities) {
+			if (namedEntity.getType().getName().equals("CM"))
+				out.write(namedEntity.getSurface() + " : " + namedEntity.getConfidence()
+						+ "\n");
+				
+		}
+			out.close();
+	}
+
+	private List<NamedEntity> getOscarEntities(String text) {
+		// TODO Auto-generated method stub
+		Oscar oscar = new Oscar();
+		List<NamedEntity> neList = new ArrayList<NamedEntity>();
+		try {
+			text = oscar.normalize(text);
+			List<ITokenSequence> tokens = oscar.tokenize(text);
+			oscar.setRecogniser(new PatternRecogniser());
+			neList = oscar.recognizeNamedEntities(tokens);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return neList;
+	}
+
+	private JCas createJCas() throws UIMAException, IOException,
+			ResourceInitializationException {
+		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
+				.createTypeSystemDescription(NamedEntity.class);
+		typeSystemDescription.setName("OscarPattern");
+		AnalysisEngine engine = AnalysisEngineFactory
+				.createAnalysisEngine(typeSystemDescription.getName());
+		JCas aJCas = engine.newJCas();
+		return aJCas;
+	}
+
+	private String getContentAsString(String pathName) throws IOException {
+
+		InputStream is = ClassLoader.getSystemResourceAsStream(pathName);
+		if (is != null)
+			return IOUtils.toString(is, "UTF-8");
+		else
+			return null;
+
+	}
+
+	private List<String> convertToStringList(
+			List<org.u_compare.shared.syntactic.Token> syntaxTokens) {
+		List<String> stringTokenList = new ArrayList<String>();
+		for (org.u_compare.shared.syntactic.Token token : syntaxTokens) {
+			stringTokenList.add(token.getCoveredText());
+		}
+
+		return stringTokenList;
+	}
+
+}
